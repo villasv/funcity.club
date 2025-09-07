b81b59ff61d78714e4f89ec87b215c2bda881dbb
Victor Villas <mail@victor.villas>
2025-09-06 19:29:27 -0700

    Expand land ack with other relevant notes

--- src/utils/city.ts
+++ src/utils/city.ts
@@ -14,11 +14,12 @@ export function getCurrentCityFromSubdomain(): CityCode | undefined {
 }
 
 const LAND_ACKNOWLEDGEMENTS = {
-  [CityCode.YVR]: `Metro Vancouver is located within the shared, unceded, and
-   ancestral territories of many Indigenous Peoples, including 10 local First
-   Nations: q̓ic̓əy̓ (Katzie), q̓ʷɑ:n̓ƛ̓ən̓ (Kwantlen), kʷikʷəƛ̓əm (Kwikwetlem), máthxwi
-   (Matsqui), xʷməθkʷəy̓əm (Musqueam), qiqéyt (Qayqayt), Semiahmoo, Sḵwx̱wú7mesh
-   Úxwumixw (Squamish), scəw̓aθən məsteyəxʷ (Tsawwassen) and səlilwətaɬ (Tsleil-Waututh).`,
+  [CityCode.YVR]: `
+Metro Vancouver is located within the shared, unceded, and ancestral territories
+of many Indigenous Peoples, including 10 local First Nations: q̓ic̓əy̓ (Katzie),
+q̓ʷɑ:n̓ƛ̓ən̓ (Kwantlen), kʷikʷəƛ̓əm (Kwikwetlem), máthxwi (Matsqui),
+xʷməθkʷəy̓əm (Musqueam), qiqéyt (Qayqayt), Semiahmoo, Sḵwx̱wú7mesh Úxwumixw
+(Squamish), scəw̓aθən məsteyəxʷ (Tsawwassen) and səlilwətaɬ (Tsleil-Waututh).`,
 
   [CityCode.YYZ]: `We acknowledge that Toronto is located on the traditional territory of many nations including the Mississaugas of the Credit, the Anishnabeg, the Chippewa, the Haudenosaunee and the Wendat peoples.`,
 };
